1fe375f661c4d272e1f28beb1b94ff53ddf5ac28
suhad shaheen <s12325463@stu.najah.edu>
2026-06-30 15:03:45 +0300

    create the structure of the landing page folder

--- frontend/info-hub/src/app/landing-page/landing-page.ts
+++ frontend/info-hub/src/app/landing-page/landing-page.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Navbar } from './components/navbar/navbar';
+import { Hero } from './components/hero/hero';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [Navbar, Hero, Footer],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.css',
 })
