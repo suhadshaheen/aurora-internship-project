8024028428925f715f75b2ebdd6d0d793758b4c7
suhad shaheen <s12325463@stu.najah.edu>
2026-07-05 12:01:40 +0300

    refactor project structure and update component naming

--- frontend/info-hub/src/app/app.ts
+++ frontend/info-hub/src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LandingPage } from './landing-page/landingPage.component';
+import { LandingPage } from './features/landing-page/landingPage.component';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, LandingPage],
