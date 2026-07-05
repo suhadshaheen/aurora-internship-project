aa7ac3715e82c23435cf25deef8504bc54aa1179
sanaassi1 <s12217470@stu.najah.edu>
2026-07-05 11:39:57 +0300

    hero constants

--- frontend/info-hub/src/app/landing-page/components/hero/hero.ts
+++ frontend/info-hub/src/app/landing-page/components/hero/hero.ts
@@ -6,4 +6,15 @@ import { CardModule } from 'primeng/card';
   templateUrl: './hero.html',
   styleUrl: './hero.css',
 })
-export class Hero {}
+export class Hero {
+  readonly title = "Your team's collective intelligence, organized.";
+
+  readonly description =
+    "Centralize documentation, onboard faster, and keep your team aligned — all in one place.";
+
+  readonly loginText = "Log In";
+
+  readonly guestText = "Continue as a Guest";
+
+  readonly arrow = "->";
+}
