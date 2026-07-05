4f99c1d2707a62de44413c51bcfd99e3dec60f2d
sanaassi1 <s12217470@stu.najah.edu>
2026-07-05 12:17:58 +0300

    new branch/login-page

---
+++ frontend/info-hub/src/app/features/login-page.component/login-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-page.component',
+  imports: [],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.css',
+})
+export class LoginPageComponent {}
