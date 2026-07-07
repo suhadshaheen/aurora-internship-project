c45a6aace423b91d986ea071ad4b30a7e473f23c
sanaassi1 <s12217470@stu.najah.edu>
2026-07-07 11:18:17 +0300

    comment component

---
+++ frontend/info-hub/src/app/src/app/pages/dashboards/components/comment.component/comment.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comment.component',
+  imports: [],
+  templateUrl: './comment.component.html',
+  styleUrl: './comment.component.css',
+})
+export class CommentComponent {}
