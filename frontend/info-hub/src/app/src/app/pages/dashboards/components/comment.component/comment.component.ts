43313d0a79507a7a32124a7ff798dd689c74f51c
suhad shaheen <s12325463@stu.najah.edu>
2026-07-07 11:23:36 +0300

    delete some useless files

--- frontend/info-hub/src/app/src/app/pages/dashboards/components/comment.component/comment.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-comment.component',
-  imports: [],
-  templateUrl: './comment.component.html',
-  styleUrl: './comment.component.css',
-})
-export class CommentComponent {}
