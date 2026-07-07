83941505ec6591f05d6b1f357422e504af91335e
suhad shaheen <s12325463@stu.najah.edu>
2026-07-07 15:22:14 +0300

    add category & section services

---
+++ frontend/info-hub/src/app/pages/dashboard/components/category/store/category.effects.ts
@@ -0,0 +1,53 @@
+import { inject, Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { catchError, map, mergeMap, of } from 'rxjs';
+import { CategoryService } from '../services/category.service';
+import { CategoryActions } from './category.actions';
+
+@Injectable()
+export class CategoryEffects {
+  private actions$ = inject(Actions);
+  private categoryService = inject(CategoryService);
+
+  loadCategories$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(CategoryActions.loadCategories),
+      mergeMap(() =>
+        this.categoryService.getAll().pipe(
+          map((categories) => CategoryActions.loadCategoriesSuccess({ categories })),
+          catchError((error) =>
+            of(CategoryActions.loadCategoriesFailure({ error: error.message })),
+          ),
+        ),
+      ),
+    ),
+  );
+
+  addCategory$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(CategoryActions.addCategory),
+      mergeMap(({ category }) =>
+        this.categoryService.create(category).pipe(
+          map((createdCategory) =>
+            CategoryActions.addCategorySuccess({ category: createdCategory }),
+          ),
+          catchError((error) => of(CategoryActions.addCategoryFailure({ error: error.message }))),
+        ),
+      ),
+    ),
+  );
+
+  deleteCategory$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(CategoryActions.deleteCategory),
+      mergeMap(({ catId }) =>
+        this.categoryService.delete(catId).pipe(
+          map(() => CategoryActions.deleteCategorySuccess({ catId })),
+          catchError((error) =>
+            of(CategoryActions.deleteCategoryFailure({ error: error.message })),
+          ),
+        ),
+      ),
+    ),
+  );
+}
---
+++ frontend/info-hub/src/app/pages/dashboard/components/section/store/section.effects.ts
@@ -0,0 +1,59 @@
+import { inject, Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { catchError, map, mergeMap, of } from 'rxjs';
+import { SectionActions } from './section.actions';
+import { SectionsService } from '../services/sections.service';
+
+@Injectable()
+export class SectionEffects {
+  private actions$ = inject(Actions);
+  private sectionsService = inject(SectionsService);
+
+  loadSections$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(SectionActions.loadSections),
+      mergeMap(() =>
+        this.sectionsService.getAll().pipe(
+          map((sections) => SectionActions.loadSectionsSuccess({ sections })),
+          catchError((error) => of(SectionActions.loadSectionsFailure({ error: error.message }))),
+        ),
+      ),
+    ),
+  );
+
+  addSection$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(SectionActions.addSection),
+      mergeMap(({ section }) =>
+        this.sectionsService.create(section).pipe(
+          map((createdSection) => SectionActions.addSectionSuccess({ section: createdSection })),
+          catchError((error) => of(SectionActions.addSectionFailure({ error: error.message }))),
+        ),
+      ),
+    ),
+  );
+
+  updateSection$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(SectionActions.updateSection),
+      mergeMap(({ section }) =>
+        this.sectionsService.update(section).pipe(
+          map((updatedSection) => SectionActions.updateSectionSuccess({ section: updatedSection })),
+          catchError((error) => of(SectionActions.updateSectionFailure({ error: error.message }))),
+        ),
+      ),
+    ),
+  );
+
+  deleteSection$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(SectionActions.deleteSection),
+      mergeMap(({ sectionId }) =>
+        this.sectionsService.delete(sectionId).pipe(
+          map(() => SectionActions.deleteSectionSuccess({ sectionId })),
+          catchError((error) => of(SectionActions.deleteSectionFailure({ error: error.message }))),
+        ),
+      ),
+    ),
+  );
+}
