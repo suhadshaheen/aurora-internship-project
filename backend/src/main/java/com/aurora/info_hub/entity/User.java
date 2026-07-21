101d2aa6c7cb310ba43d1eb74952c82e60195cac
suhad shaheen <s12325463@stu.najah.edu>
2026-07-21 14:08:24 +0300

    database creation

--- backend/src/main/java/com/aurora/info_hub/entity/User.java
+++ backend/src/main/java/com/aurora/info_hub/entity/User.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Table(name = "users")
 public class User{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
