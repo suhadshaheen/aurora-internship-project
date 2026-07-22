c93ff0abfb2be8a2112284c0921b8d240e9b78f7
sanaassi1 <s12217470@stu.najah.edu>
2026-07-22 14:00:37 +0300

    editing user entity

--- backend/src/main/java/com/aurora/info_hub/entity/User.java
+++ backend/src/main/java/com/aurora/info_hub/entity/User.java
@@ -15,7 +15,7 @@ import lombok.NoArgsConstructor;
 public class User{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     @Column(nullable = false, unique = true)
     String userName;
     @Column(nullable = false)
--- backend/src/main/java/com/aurora/info_hub/service/CommentService.java
+++ backend/src/main/java/com/aurora/info_hub/service/CommentService.java
@@ -18,7 +18,7 @@ public class CommentService {
     }
 
 
-
+ 
     // GET all comments
     public List<Comment> getAllComments(){
 
