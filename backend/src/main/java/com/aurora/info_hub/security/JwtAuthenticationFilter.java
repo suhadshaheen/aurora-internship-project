d4f3c5df27942fa8fc2e7c2a5dece7dfba69c9f8
sanaassi1 <s12217470@stu.najah.edu>
2026-07-28 14:16:39 +0300

    jwt filter '

--- backend/src/main/java/com/aurora/info_hub/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/aurora/info_hub/security/JwtAuthenticationFilter.java
@@ -1,6 +1,19 @@
 package com.aurora.info_hub.security;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.security.web.authentication.OncePerRequestFilter;
+import org.springframework.stereotype.Component;
+import java.io.IOException;
+import com.aurora.info_hub.service.JwtService;
 
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    
+    private final JwtService jwtService;
 }
