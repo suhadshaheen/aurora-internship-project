186bf57dbbda248d72785770beeef174a138bf8b
suhad shaheen <s12325463@stu.najah.edu>
2026-07-28 16:59:16 +0300

    add jwt authController

--- backend/src/main/java/com/aurora/info_hub/security/securityConfig.java
+++ backend/src/main/java/com/aurora/info_hub/security/securityConfig.java
@@ -41,6 +41,7 @@ public class securityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated()
