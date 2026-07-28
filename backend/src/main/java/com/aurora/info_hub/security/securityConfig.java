4cfe9f71bbe6d4fcbb3d6fad739470278eece93e
suhad shaheen <s12325463@stu.najah.edu>
2026-07-28 17:10:05 +0300

    add jwt authController

--- backend/src/main/java/com/aurora/info_hub/security/securityConfig.java
+++ backend/src/main/java/com/aurora/info_hub/security/securityConfig.java
@@ -3,7 +3,7 @@ import com.aurora.info_hub.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.CachingUserDetailsService;
+
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
+@Configuration
 public class securityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private  final CustomUserDetailsService customUserDetailsService;
