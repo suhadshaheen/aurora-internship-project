b8dbcd7f57d06e1970f7cc131f1faf2d430db94b
sanaassi1 <s12217470@stu.najah.edu>
2026-07-30 19:09:22 +0300

    new apis

--- frontend/src/app/pages/dashboard/components/comment/services/comment.service.ts
+++ frontend/src/app/pages/dashboard/components/comment/services/comment.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IComment } from '../../../../../../models/comment.interface';
+import { environment } from '../../../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CommentService {
-  private readonly apiUrl = 'http://localhost:3000/comments';
+  private readonly apiUrl = environment.apiUrl + '/comments';
 
   constructor(private http: HttpClient) {}
 
