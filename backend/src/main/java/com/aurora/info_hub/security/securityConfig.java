fa2082e1a204495574309195ebffc6010573c697
suhad shaheen <s12325463@stu.najah.edu>
2026-08-02 11:34:27 +0300

    Enable CORS in backend

--- backend/src/main/java/com/aurora/info_hub/security/securityConfig.java
+++ backend/src/main/java/com/aurora/info_hub/security/securityConfig.java
@@ -62,6 +62,7 @@ public AuthenticationManager authenticationManager(AuthenticationConfiguration c
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/sections/**").permitAll()
