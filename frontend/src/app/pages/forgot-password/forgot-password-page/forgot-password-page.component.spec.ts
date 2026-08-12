cfd505a793ec0cd0876442c72fb4d2ea6cf9acfe
suhad shaheen <s12325463@stu.najah.edu>
2026-08-12 14:54:53 +0300

    test the sidebar, admin view , auth.guard and admin.guard files

--- frontend/src/app/pages/forgot-password/forgot-password-page/forgot-password-page.component.spec.ts
+++ frontend/src/app/pages/forgot-password/forgot-password-page/forgot-password-page.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MessageService } from 'primeng/api';
+import { provideMockStore } from '@ngrx/store/testing';
 
 import { ForgotPasswordPage } from './forgot-password-page.component';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
 
 describe('ForgotPasswordPage', () => {
   let component: ForgotPasswordPage;
@@ -9,11 +12,21 @@ describe('ForgotPasswordPage', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ForgotPasswordPage],
+      providers: [
+        MessageService,
+        provideMockStore({ initialState: {} }),
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: { queryParamMap: convertToParamMap({}) },
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ForgotPasswordPage);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.detectChanges();
   });
 
   it('should create', () => {
